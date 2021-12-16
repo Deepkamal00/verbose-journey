9333c791787f861c7de821a4a194aa65c3483e61
Deepkamal <kdeep2299@gmail.com>
2021-12-16 03:13:32 -0600

    Added provinces to the active admin

---
+++ app/admin/provincs.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Provinc do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :name, :pst, :hst, :gst, :gsst
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:name, :pst, :hst, :gst, :gsst]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+
+end
