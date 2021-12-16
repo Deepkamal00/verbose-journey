b39b2e51c08c9cb2cd1d89be6a136df8757045e6
Deepkamal <kdeep2299@gmail.com>
2021-12-16 07:31:39 -0600

    Created flash message for whenever any product is added to the cart

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -3,6 +3,8 @@ class CartController < ApplicationController
     logger.debug("Adding #{params[:id]} to cart.")
     id = params[:id].to_i
     session[:shopping_cart] << id
+    product = Product.find(id)
+    flash[:notice] = "✔ #{product.title} added to cart."
     redirect_to root_path
   end
 
