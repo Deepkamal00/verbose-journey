aa0221804a07a64524e424cc51765d7e6fc79307
Deepkamal <kdeep2299@gmail.com>
2021-12-16 07:21:25 -0600

    The add to cart button is adding products to cart, but is not showing anything

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,6 +1,9 @@
 class CartController < ApplicationController
   def create
     logger.debug("Adding #{params[:id]} to cart.")
+    id = params[:id].to_i
+    session[:shopping_cart] << id
+    redirect_to root_path
   end
 
   def destroy
