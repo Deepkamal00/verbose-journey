384aa8fbbae2cf742079118705b1757fefa7c425
Deepkamal <kdeep2299@gmail.com>
2021-12-16 05:29:51 -0600

    Created people model to add users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :people
   get 'pages/:permalink' => "pages#permalink", as: 'permalink'
   resources :provincs
   devise_for :admin_users, ActiveAdmin::Devise.config
